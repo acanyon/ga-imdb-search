a56719f2a4902717c04c8f7d1c9b351a6849ced5
Amanda Canyon <acanyon@gmail.com>
2016-08-21 10:41:56 -0700

    wip + starter code

---
+++ app.rb
@@ -0,0 +1,17 @@
+require 'sinatra'
+
+get '/' do
+  File.read(File.join('views', 'index.html'))
+end
+
+get '/favorites' do
+  response.header['Content-Type'] = 'application/json'
+  file = JSON.parse(File.read('data.json'))
+  unless params[:name] && params[:oid]
+    return 'Invalid Request'
+  end
+  movie = { name: params[:name], oid: params[:oid] }
+  file << movie
+  File.write('data.json',JSON.pretty_generate(file))
+  movie.to_json
+end
